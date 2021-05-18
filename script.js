de48374c2b99efe1eb601f8d1a441c818aba5be4
unknown <sieonpark1308@gmail.com>
2021-05-18 11:48:53 +0900

    Sieon Assignment

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
+const name = 'My name is \sieon\.';
+const age = 'I am\ 20\ years old.';
 
-const mass_Mark = 78;
-const height_Mark = 1.69;
-const mass_John = 92;
-const height_John = 1.95;
-
-const BMI_Mark = mass_Mark / (height_Mark ** 2);
-const BMI_John = mass_John / (height_John ** 2);
-
-
-if(BMI_Mark > BMI_John){
-    console.log(`Mark's BMI (${BMI_Mark}) is higher than John's BMI (${BMI_John})`);
-}else{
-    console.log(`John's BMI (${BMI_John}) is higher than Mark's BMI (${BMI_Mark})`);
-}
+console.log(name);
+console.log(age);
