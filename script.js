d76eb174478c49a1f1b623d88c6376d92bc4b856
Sieon-Park <sieonpark1308@gmail.com>
2021-05-18 14:21:24 +0900

    Sieon Assignment

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ const age = 'I am\ 20\ years old.';
 
 console.log(name);
 console.log(age);
+
+
+
+const myname = "sieon";
+const myage = 20;
+const myheight = 169;
+const averageweight = ((myheight - 100) * 0.9);
+const result = `My name is ${myname} and I am ${myage} years old and My averageweight is ${averageweight}`;
+
+console.log(result);
+
