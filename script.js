4e83622110ab51c5c35f2503533fc62160edd069
spark131008 <spark131008@gmail.com>
2021-05-19 23:55:55 -0400

    html and js files updated

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 'use strict';
 
+const temps = [12, 5, -5, 0, 4];
+
+const printForecast = function(arr){
+    let finalString = '...';
+    for(let i=0; i<temps.length; i++){
+        finalString += ` ${temps[i]}C in ${i+1} days ...`;
+    }
+    return finalString;
+}
+
+console.log(printForecast(temps));
