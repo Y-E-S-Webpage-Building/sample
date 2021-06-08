49dd0c4663c4ba67c86d3788bddb7873903f6c75
Jinyoung Jeon <83676788+acnove53@users.noreply.github.com>
2021-06-08 23:48:37 +0900

    h

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function functionForbutton() {
     const name = document.getElementById('textt').value;
     document.getElementById("result").innerText = textt;
   }
+function printName() {
+    const name = document.getElementById('textt').value;
+    document.getElementById("result").innerText = name;
+}
