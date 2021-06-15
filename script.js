4f880650cb3815ec4e5adcac1c4995cd05532b77
Sieon-Park <sieonpark1308@gmail.com>
2021-06-15 21:51:50 +0900

    dede

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-const name = 'My name is \sieon\.';
-const age = 'I am\ 20\ years old.';
-
-console.log(name);
-console.log(age);
-
-
-
-const myname = "sieon";
-const myage = 20;
-const myheight = 169;
-const averageweight = ((myheight - 100) * 0.9);
-const result = `My name is ${myname} and I am ${myage} years old and My averageweight is ${averageweight}`;
-
-console.log(result);
 
 function monthlyTotal() {
     //First, get to the active spreadsheet
