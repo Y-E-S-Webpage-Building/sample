3ea54267a16cfbd9510c470cd84a4ea0d3d1a85c
Sieon-Park <sieonpark1308@gmail.com>
2021-06-15 21:49:20 +0900

    practicee

--- script.js
+++ script.js
@@ -43,3 +43,19 @@ function monthlyTotal() {
     }
   
   }
+
+  function onEdit(e) {
+    var ss = SpreadsheetApp.getActiveSpreadsheet();
+    var sheet = ss.getActiveSheet();
+    //checks that we're on the correct sheet.
+    if( sheet.getSheetName() == 'Timesheet Program' || 'Cash Payment Tracker') { 
+      var selectedCell = ss.getActiveCell();
+      //checks the column to ensure it is on the one we want to cause the date to appear.
+      if( selectedCell.getColumn() == 2) { 
+        var dateTimeCell1 = selectedCell.offset(0,1);
+        dateTimeCell1.setValue(new Date());
+        var dateTimeCell2 = selectedCell.offset(0,-1);
+        dateTimeCell2.setValue(new Date());
+        }
+    }
+  }
