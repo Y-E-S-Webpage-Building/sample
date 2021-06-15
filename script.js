2778a1a92f95218861aeef979fd66786c50118f8
Jinyoung Jeon <83676788+acnove53@users.noreply.github.com>
2021-06-15 23:31:29 +0900

    b

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function printBoth() {
+function printtheinput() {
     const name = document.getElementById('textt').value;
     document.getElementById("result").innerText = name;
+}
+
+function printtheinput2() {
+    const name = document.getElementById('numberr').value;
+    document.getElementById("result2").innerText = age;
 }
